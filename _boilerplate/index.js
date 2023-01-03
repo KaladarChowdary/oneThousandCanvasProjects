ec0dc4c440edee75e8d8a27bd14d283f946a4dfd
kaladar <ckaladar@gmail.com>
2023-01-03 19:19:23 +0530

    updated boilerPlate

--- _boilerplate/index.js
+++ _boilerplate/index.js
@@ -1,24 +1,21 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
 // WRITE ALL FUNCTIONS HERE
 function maxifyCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
 
-function clearScreen(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+function clearScreen() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    clearScreen()
+function animate() {
+  requestAnimationFrame(animate);
+  clearScreen();
 }
 // CODE TO RUN
 maxifyCanvas();
 
-
-
-function ani
+animate();
