68454ef7210183b3d7a4c9665f37a5aeb8498d66
kaladar <ckaladar@gmail.com>
2023-01-03 19:24:57 +0530

    Successfully created sequential boxes

--- project 3/index.js
+++ project 3/index.js
@@ -11,9 +11,19 @@ function clearScreen() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function changeWithWindow() {
+  window.addEventListener("resize", maxifyCanvas);
+}
+
 function animate() {
   requestAnimationFrame(animate);
   clearScreen();
+
+  for (let i = 0; i + 6 <= canvas.width; i += 6) {
+    for (let j = 0; j + 6 <= canvas.height; j += 6) {
+      ctx.fillRect(i, j, 5, 5);
+    }
+  }
 }
 // CODE TO RUN
 maxifyCanvas();
