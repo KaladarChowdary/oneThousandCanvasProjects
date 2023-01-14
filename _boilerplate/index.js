7d79d8d5c035094689eb54f25d53eb39e0b2db7b
kaladar <ckaladar@gmail.com>
2023-01-14 07:34:46 +0530

    updating boiler plate to order entities of javascript

--- _boilerplate/index.js
+++ _boilerplate/index.js
@@ -1,7 +1,18 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-// WRITE ALL FUNCTIONS HERE
+//
+//
+//
+//
+// Classes are declared here
+//
+//
+//
+//
+// Event Listeners are declared here
+//
+//
+//
+//
+// Functions are declared here
 function maxifyCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -15,7 +26,19 @@ function animate() {
   requestAnimationFrame(animate);
   clearScreen();
 }
-// CODE TO RUN
-maxifyCanvas();
 
+//
+//
+//
+//
+// Variables are declared here
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+//
+//
+//
+//
+// functions are run here
+
+maxifyCanvas();
 animate();
