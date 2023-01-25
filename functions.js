a05cd080f4583931916524873164b3e9a5721487
kaladar <ckaladar@gmail.com>
2023-01-25 16:28:08 +0530

    Added centre point

--- functions.js
+++ functions.js
@@ -1,5 +1,70 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// FUNCTIONS
+function maxify() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function middleX() {
+  return canvas.width / 2;
+}
+
+function middleY() {
+  return canvas.height / 2;
+}
+
+function endX() {
+  return canvas.width;
+}
+
+function endY() {
+  return canvas.height;
+}
+
 function getDistance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function randElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 function randRange(min, max) {
@@ -10,6 +75,115 @@ function randInt(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-function randItem(arr) {
-  return arr[randInt(0, arr.length - 1)];
+function positive(num) {
+  return Math.abs(num);
+}
+
+function negative(num) {
+  return -Math.abs(num);
+}
+
+function insideCircle(x1, y1, x2, y2, r2) {
+  return getDistance(x1, y1, x2, y2) <= r2;
+}
+
+function fillCanvas(color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, endX(), endY());
+}
+
+function giveCoordinates(size, gap) {
+  let arr = [];
+  let x = 0;
+  let y = 0;
+  let next = size + gap;
+
+  for (let i = 0; y < canvas.height; i++) {
+    y = i * next;
+    for (let j = 0; x < canvas.width; j++) {
+      x = j * next;
+      arr.push([x, y]);
+    }
+    x = 0;
+  }
+
+  return arr;
+}
+
+function makeSquares(arr, size) {
+  let arr2 = [];
+
+  arr.forEach(([x, y]) => {
+    arr2.push(new Square(x, y, size));
+  });
+
+  return arr2;
+}
+
+function updateAll(arr) {
+  arr.forEach((element) => {
+    element.update();
+  });
+}
+
+function onSquare(x1, y1, x2, y2, size2) {
+  if (x1 < x2 || x1 > x2 + size2 || y1 < y2 || y1 > y2 + size2) {
+    return false;
+  }
+  return true;
+}
+
+function isInsideCircle(x1, y1, x2, y2, r2) {
+  return getDistance(x1, y1, x2, y2) <= r2;
+}
+// 0 - 360 : ANGLE BETWEEN TWO POINTS
+
+function getAngleInDegrees(x1, y1, x2, y2) {
+  return radToDeg(getAngle(x1, y1, x2, y2));
+}
+function getAngle(x1, y1, x2, y2) {
+  let xdiff, ydiff, theta, updatedTheta;
+  xdiff = dx(x1, x2);
+  ydiff = dy(y1, y2);
+  theta = getTheta(xdiff, ydiff);
+  return updateThetaWithQuadrants(x1, y1, x2, y2, theta);
+}
+
+function dx(x1, x2) {
+  return Math.abs(x1 - x2);
+}
+
+function dy(y1, y2) {
+  return Math.abs(y1 - y2);
+}
+
+function getTheta(dx, dy) {
+  return Math.atan2(dy, dx);
+}
+
+function radToDeg(angle) {
+  return (angle * 180) / Math.PI;
+}
+
+function updateThetaWithQuadrants(x1, y1, x2, y2, theta) {
+  let i = getQuadrant(x1, y1, x2, y2);
+
+  if (i === 1) {
+    theta = theta;
+  } else if (i === 2) {
+    theta = Math.PI - theta;
+  } else if (i === 3) {
+    theta = Math.PI + theta;
+  } else if (i === 4) {
+    theta = 2 * Math.PI - theta;
+  }
+
+  return theta;
+}
+
+function getQuadrant(x1, y1, x2, y2) {
+  if (x2 > x1 && y2 <= y1) return 1;
+  else if (x2 <= x1 && y2 < y1) return 2;
+  else if (x2 < x1 && y2 >= y1) return 3;
+  else if (x2 >= x1 && y2 > y1) return 4;
 }
